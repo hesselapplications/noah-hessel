0e73025718619f8ea58a3e6c119973c4075358ee
Noah Hessel <hesselapplications@gmail.com>
2020-12-08 16:05:07 -0700

    Add web share button

---
+++ src/components/Share.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-fab-transition v-if="shareSupported">
+    <v-btn color="primary" fab dark bottom right fixed @click="share">
+      <v-icon class="pr-1">mdi-share-variant</v-icon>
+    </v-btn>
+  </v-fab-transition>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  },
+  computed: {
+    shareSupported() {
+      return navigator.share;
+    },
+  },
+  methods: {
+    share() {
+      navigator.share({
+        url: location.href,
+        title: this.title,
+      });
+    },
+  },
+};
+</script>
--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -8,17 +8,17 @@
           <v-card>
             <image-viewer :src="post.src"></image-viewer>
             <v-divider></v-divider>
-            <v-card-title class="pb-0">
+            <v-card-title class="text-h4 pb-0">
               {{ post.title }}
             </v-card-title>
-            <v-card-text>{{ datePosted }}</v-card-text>
-            <v-divider></v-divider>
+            <v-card-text class="caption">{{ datePosted }}</v-card-text>
             <v-card-text>
               <block-content :blocks="post.body" />
             </v-card-text>
           </v-card>
         </v-col>
       </v-row>
+      <share v-bind="post"></share>
     </v-container>
   </div>
 </template>
@@ -28,12 +28,14 @@ import moment from "moment";
 import ImageViewer from "@/components/ImageViewer";
 import BlockContent from "@/components/BlockContent";
 import AppNavigation from "@/components/AppNavigation";
+import Share from "@/components/Share.vue";
 
 export default {
   components: {
     ImageViewer,
     BlockContent,
-    AppNavigation
+    AppNavigation,
+    Share,
   },
   props: {
     slug: null,
@@ -45,7 +47,7 @@ export default {
   },
   computed: {
     datePosted() {
-      return moment(this.post.datePosted).format("MMM Do YYYY");
+      return moment(this.post.datePosted).format("MMMM Do YYYY");
     },
   },
   async created() {
