6c7c13364c71a84a07e72984bf1cd2663412721f
Noah Hessel <hesselapplications@gmail.com>
2020-12-08 16:55:48 -0700

    Soften up those skills!

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -4,7 +4,7 @@
       <v-icon :color="model.color" class="pa-3">{{model.icon}}</v-icon>
       <span class="text-body-2 white--text">{{model.label}}</span>
     </div>
-    <v-progress-linear :value="model.level" :color="model.color" height="8"></v-progress-linear>
+    <v-progress-linear rounded :value="model.level" :color="model.color" height="8"></v-progress-linear>
   </div>
 </template>
 
