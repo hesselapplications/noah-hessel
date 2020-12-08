4e0f443cc57ecbd99da648477d12a4fd20eed5bc
Noah Hessel <hesselapplications@gmail.com>
2020-12-08 16:21:55 -0700

    Make share button smaller on mobile

--- src/components/Share.vue
+++ src/components/Share.vue
@@ -1,6 +1,6 @@
 <template>
   <v-fab-transition v-if="shareSupported">
-    <v-btn color="primary" fab dark bottom right fixed @click="share">
+    <v-btn :small="$vuetify.breakpoint.smAndDown" color="secondary" fab dark bottom right fixed @click="share">
       <v-icon class="pr-1">mdi-share-variant</v-icon>
     </v-btn>
   </v-fab-transition>
